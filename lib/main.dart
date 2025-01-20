c30cb68794fea10b7df2ef61b5d2fb44e7db04c9
mohamed hosny <mohamed@gmail.com>
2025-01-20 12:01:39 +0200

    add screenUtile

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tasky/tasky.dart';
 import 'package:tasky/tasky_enjection.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
   await TaskyEnjection().init();
   runApp(const Tasky());
 }
